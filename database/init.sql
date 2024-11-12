1b94232fc93425b5256ac92e8e4e7ccc7ef78ca8
Guillem Poy <guplem@gmail.com>
2024-11-12 20:50:46 +0100

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,2 @@
 CREATE DATABASE IF NOT EXISTS mydb;
 USE mydb;
-
-CREATE TABLE IF NOT EXISTS posts (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255),
-  body TEXT
-);
-
-INSERT INTO posts (title, body) VALUES ('First Post', 'This is the first post');
