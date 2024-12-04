f49abcfedd645b100263632ca2ac3a1a093acde4
Guillem Poy <guplem@gmail.com>
2024-12-04 14:10:49 +0100

    End state of live coding

--- dashboard/src/modules/projects/projectList.tsx
+++ dashboard/src/modules/projects/projectList.tsx
@@ -1,13 +1,66 @@
-import { List, Datagrid, TextField, ChipField } from "react-admin";
+import { List, Datagrid, TextField, ChipField, BulkUpdateButton, BulkDeleteButton, BulkExportButton, BulkActionsToolbar } from "react-admin";
 import { PercentageField } from "../../utils/components/percentageField";
+import { Button } from "@mui/material";
+import { useListContext } from "react-admin";
+import jsPDF from "jspdf";
 
-export const ProjectList = () => (
-  <List>
-    <Datagrid>
-      {/* <TextField source="id" /> */}
-      <TextField source="name" />
-      <ChipField source="type" />
-      <PercentageField source="conformityProgress" decimalPlaces={1} />
-    </Datagrid>
-  </List>
+const PostBulkActionButtons = () => (
+  <>
+    <BulkActionsToolbar />
+    <BulkDeleteButton />
+    <BulkExportButton />
+  </>
 );
+
+const fakeData = [
+  {
+    id: 1,
+    name: "Project Alpha",
+    type: "Development",
+    conformityProgress: 0.85,
+  },
+  {
+    id: 2,
+    name: "Project Beta",
+    type: "Research",
+    conformityProgress: 0.75,
+  },
+  {
+    id: 3,
+    name: "Project Gamma",
+    type: "Testing",
+    conformityProgress: 0.95,
+  },
+];
+
+export const ProjectList = () => {
+  return (
+    <>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          const doc = new jsPDF();
+
+          doc.autoTable({
+            head: [["ID", "Name", "Type", "Conformity Progress"]],
+            body: fakeData.map((row) => [row.id, row.name, row.type, row.conformityProgress]),
+          });
+
+          doc.save("table.pdf");
+        }}
+      >
+        Export PDF
+      </Button>
+
+      <List>
+        <Datagrid bulkActionButtons={<PostBulkActionButtons />}>
+          {/* <TextField source="id" /> */}
+          <TextField source="name" />
+          <ChipField source="type" />
+          <PercentageField source="conformityProgress" decimalPlaces={1} />
+        </Datagrid>
+      </List>
+    </>
+  );
+};
